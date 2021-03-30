e9765dd9bc7d955a4e2a81507d6165e2d73c08df
E.Z. Hart <hartez@gmail.com>
2021-03-30 15:05:39 -0600

    Fixed Label text change layout update issues

--- src/Core/src/Handlers/Label/LabelHandler.Android.cs
+++ src/Core/src/Handlers/Label/LabelHandler.Android.cs
@@ -3,6 +3,7 @@ using Microsoft.Maui.Graphics;
 using Android.Content;
 using Android.Runtime;
 using Android.Util;
+using Android.Views;
 using Android.Widget;
 using Java.Lang;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,7 +16,7 @@ namespace Microsoft.Maui.Handlers
 		static float LineSpacingAddDefault { get; set; }
 		static float LineSpacingMultDefault { get; set; }
 
-		protected override TextView CreateNativeView() => new MyTextView(Context);
+		protected override TextView CreateNativeView() => new TextView(Context);
 
 		protected override void SetupDefaults(TextView nativeView)
 		{
@@ -27,8 +28,11 @@ namespace Microsoft.Maui.Handlers
 			{
 				DefaultTextColor = Color.FromUint((uint)nativeView.TextColors.DefaultColor);
 			}
+
 			LineSpacingAddDefault = nativeView.LineSpacingExtra;
 			LineSpacingMultDefault = nativeView.LineSpacingMultiplier;
+
+			nativeView.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
 		}
 
 		public static void MapText(LabelHandler handler, ILabel label)
