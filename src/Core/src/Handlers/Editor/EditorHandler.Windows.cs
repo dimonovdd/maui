621607af8d5dfa2877d82f4d95f85b236c2fe936
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-14 10:12:04 +0200

    Implement basic Editor properties in WinUI

--- src/Core/src/Handlers/Editor/EditorHandler.Windows.cs
+++ src/Core/src/Handlers/Editor/EditorHandler.Windows.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Handlers
 {
 	public partial class EditorHandler : ViewHandler<IEditor, TextBox>
 	{
-		protected override TextBox CreateNativeView() => new TextBox();
+		protected override TextBox CreateNativeView() => new TextBox
+		{
+			AcceptsReturn = true,
+			TextWrapping = TextWrapping.Wrap,
+			VerticalContentAlignment = VerticalAlignment.Top
+		};
 
-		[MissingMapper]
-		public static void MapText(IViewHandler handler, IEditor editor) { }
+		public static void MapText(EditorHandler handler, IEditor editor)
+		{
+			handler.NativeView?.UpdateText(editor);
+		}
 
-		[MissingMapper]
-		public static void MapPlaceholder(IViewHandler handler, IEditor editor) { }
+		public static void MapPlaceholder(EditorHandler handler, IEditor editor)
+		{
+			handler.NativeView?.UpdatePlaceholder(editor);
+		}
 
 		[MissingMapper]
 		public static void MapPlaceholderColor(IViewHandler handler, IEditor editor) { }
@@ -19,17 +28,25 @@ namespace Microsoft.Maui.Handlers
 		[MissingMapper]
 		public static void MapCharacterSpacing(IViewHandler handler, IEditor editor) { }
 
-		[MissingMapper]
-		public static void MapMaxLength(IViewHandler handler, IEditor editor) { }
+		public static void MapMaxLength(EditorHandler handler, IEditor editor)
+		{
+			handler.NativeView?.UpdateMaxLength(editor);
+		}
 
 		[MissingMapper]
 		public static void MapIsTextPredictionEnabled(EditorHandler handler, IEditor editor) { }
 
-		[MissingMapper]
-		public static void MapFont(IViewHandler handler, IEditor editor) { }
+		public static void MapFont(EditorHandler handler, IEditor editor)
+		{
+			var fontManager = handler.GetRequiredService<IFontManager>();
 
-		[MissingMapper]
-		public static void MapIsReadOnly(IViewHandler handler, IEditor editor) { }
+			handler.NativeView?.UpdateFont(editor, fontManager);
+		}
+
+		public static void MapIsReadOnly(EditorHandler handler, IEditor editor)
+		{
+			handler.NativeView?.UpdateIsReadOnly(editor);
+		}
 
 		[MissingMapper]
 		public static void MapTextColor(EditorHandler handler, IEditor editor) { }
---
+++ src/Core/src/Platform/Windows/TextBoxExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.Maui
+{
+	public static class TextBoxExtensions
+	{
+		public static void UpdateText(this TextBox nativeControl, IEditor editor)
+		{
+			string newText = editor.Text;
+
+			if (nativeControl.Text == newText)
+				return;
+			
+			nativeControl.Text = newText;
+			nativeControl.SelectionStart = nativeControl.Text.Length;
+		}
+
+		public static void UpdatePlaceholder(this TextBox nativeControl, IEditor editor)
+		{
+			nativeControl.PlaceholderText = editor.Placeholder ?? string.Empty;
+		}
+		public static void UpdateFont(this TextBox nativeControl, IText text, IFontManager fontManager) =>
+			nativeControl.UpdateFont(text.Font, fontManager);
+
+		public static void UpdateFont(this TextBox nativeControl, Font font, IFontManager fontManager)
+		{
+			nativeControl.FontSize = fontManager.GetFontSize(font);
+			nativeControl.FontFamily = fontManager.GetFontFamily(font);
+			nativeControl.FontStyle = font.FontAttributes.ToFontStyle();
+			nativeControl.FontWeight = font.FontAttributes.ToFontWeight();
+		}
+
+		public static void UpdateMaxLength(this TextBox nativeControl, IEditor editor)
+		{
+			nativeControl.MaxLength = editor.MaxLength;
+
+			var currentControlText = nativeControl.Text;
+
+			if (currentControlText.Length > editor.MaxLength)
+				nativeControl.Text = currentControlText.Substring(0, editor.MaxLength);
+		}
+
+		public static void UpdateIsReadOnly(this TextBox nativeControl, IEditor editor)
+		{
+			nativeControl.IsReadOnly = editor.IsReadOnly;
+		}
+	}
+}
